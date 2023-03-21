818c030534c3299b8771bfe9737d8135e03b87f0
Otso Helos <17408120+otsohelos@users.noreply.github.com>
2023-03-21 14:56:01 +0200

    creating a new user works

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
 from flask import Flask
+from flask import render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///otsohelos"
+db = SQLAlchemy(app)
+1
 @app.route("/")
 def index():
-    return "Heipparallaa!"
+    result = db.session.execute(text("SELECT * FROM users"))
+    names = result.fetchall()
+    return render_template("index.html", names=names)
 
-@app.route("/page1")
-def page1():
-    return "Tämä on sivu 1"
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
-@app.route("/page2")
-def page2():
-    return "Tämä on sivu 2"
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/signupsubmit", methods=["POST"])
+def signupsubmit():
+    username = request.form["username"]
+    password = request.form["password"]
+    userlevel="user"
+    print(password)
+    print(username)
+    hash_value = generate_password_hash(password)
+    sql = text("INSERT INTO users (username, password, userlevel) VALUES (:username, :password, :userlevel)")
+    db.session.execute(sql, {"username":username, "password":hash_value, "userlevel":userlevel})
+    db.session.commit()
+    return redirect("success.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    return render_template("result.html", name=request.form["name"])

665101a6a4994875f8ae766cc758fd43c390ccdd
Otso Helos <17408120+otsohelos@users.noreply.github.com>
2023-03-22 12:42:02 +0200

    login and logout work, doesnt check password yet

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from werkzeug.security import check_password_hash, generate_password_hash
+from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///otsohelos"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
-1
+app.secret_key = getenv("SECRET_KEY")
+
+
 @app.route("/")
 def index():
     result = db.session.execute(text("SELECT * FROM users"))
-    names = result.fetchall()
-    return render_template("index.html", names=names)
+    return render_template("index.html")
 
 @app.route("/login")
 def login():
@@ -22,18 +24,28 @@ def login():
 def signup():
     return render_template("signup.html")
 
+@app.route("/loginsubmit", methods=["POST"])
+def loginsubmit():
+    username = request.form["username"]
+    password = request.form["password"]
+    session["username"] = username
+    return redirect("/")
+
 @app.route("/signupsubmit", methods=["POST"])
 def signupsubmit():
     username = request.form["username"]
     password = request.form["password"]
     userlevel="user"
-    print(password)
-    print(username)
     hash_value = generate_password_hash(password)
     sql = text("INSERT INTO users (username, password, userlevel) VALUES (:username, :password, :userlevel)")
     db.session.execute(sql, {"username":username, "password":hash_value, "userlevel":userlevel})
     db.session.commit()
     return redirect("/success")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
 
 @app.route("/success")
 def success():

b81dd2be0db00e9f8f2feadfe02e9cd85b19c91b
Otso Helos <17408120+otsohelos@users.noreply.github.com>
2023-04-11 13:36:39 +0300

    style fixes after pylint check

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import getenv
 import datetime
 import pytz
 from flask import Flask, url_for
@@ -5,7 +6,6 @@ from flask import render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from werkzeug.security import check_password_hash, generate_password_hash
-from os import getenv
 
 # initialize database
 app = Flask(__name__)
@@ -19,6 +19,7 @@ cache["errormessage"] = ""
 cache["content"] = ""
 cache["header"] = ""
 cache["reply_to_edit"] = 0
+
 
 # Main, thread , and user pages
 @app.route("/")
@@ -37,75 +38,93 @@ def newconversation():
     message_header = cache["header"]
     cache["header"] = ""
     categories = db.session.execute(text("SELECT * from CATEGORIES"))
-    return render_template("newconversation.html", errormessage=errormessage, content=message_content, categories=categories, header = message_header)
+    return render_template(
+        "newconversation.html",
+        errormessage=errormessage,
+        content=message_content,
+        categories=categories,
+        header=message_header,
+    )
 
 
-@app.route("/thread/<int:id>")
-def thread(id):
+@app.route("/thread/<int:thread_id>")
+def thread(thread_id):
     errormessage = cache["errormessage"]
     cache["errormessage"] = ""
     sql = text(
-        "SELECT username, header, content, id FROM conversations WHERE id=:id")
-    result = db.session.execute(sql, {"id": id})
+        "SELECT * FROM conversations LEFT JOIN categories ON conversations.category=categories.id WHERE conversations.id=:thread_id")
+    result = db.session.execute(sql, {"thread_id": thread_id})
     message = result.fetchone()
     sql = text(
-        "SELECT * from replies where thread_id=:id AND deleted_at IS NULL ORDER BY id")
-    result = db.session.execute(sql, {"id": id})
+        "SELECT * from replies where thread_id=:thread_id AND deleted_at IS NULL ORDER BY id"
+    )
+    result = db.session.execute(sql, {"thread_id": thread_id})
     replies = result.fetchall()
     reply_to_edit = cache["reply_to_edit"]
     cache["reply_to_edit"] = 0
     content = cache["content"]
     cache["content"] = ""
-    return render_template("thread.html", message=message, errormessage=errormessage, replies=replies, reply_to_edit=reply_to_edit, content=content)
+    return render_template(
+        "thread.html",
+        message=message,
+        errormessage=errormessage,
+        replies=replies,
+        reply_to_edit=reply_to_edit,
+        content=content,
+    )
 
 
 @app.route("/category/<int:category_id>")
 def category(category_id):
-    sql = text("SELECT * FROM conversations LEFT JOIN categories ON conversations.category=categories.id WHERE category=:category_id ORDER BY conversations.id")
+    sql = text(
+        "SELECT * FROM conversations LEFT JOIN categories ON conversations.category=categories.id WHERE category=:category_id ORDER BY conversations.id"
+    )
     result = db.session.execute(sql, {"category_id": category_id})
     conversations = result.fetchall()
     sql = text("SELECT display_name FROM categories WHERE id=:category_id")
     result = db.session.execute(sql, {"category_id": category_id})
-    category = result.fetchone().display_name
-    return render_template("category.html", conversations=conversations, category=category)
+    category_name = result.fetchone().display_name
+    return render_template("category.html",
+                           conversations=conversations,
+                           category=category_name)
 
 
-@ app.route("/user/<string:username>")
+@app.route("/user/<string:username>")
 def user(username):
     sql = text("SELECT * FROM users WHERE username=:username")
     result = db.session.execute(sql, {"username": username})
-    user = result.fetchone()
-    return render_template("user.html", user=user)
+    current_user = result.fetchone()
+    return render_template("user.html", user=current_user)
 
 
 # Login, signup, logout routes
-@ app.route("/login")
+@app.route("/login")
 def login():
     errormessage = cache["errormessage"]
     cache["errormessage"] = ""
     return render_template("login.html", errormessage=errormessage)
 
 
-@ app.route("/signupsuccess")
+@app.route("/signupsuccess")
 def signupsuccess():
     return render_template("signupsuccess.html")
 
 
-@ app.route("/signup")
+@app.route("/signup")
 def signup():
     errormessage = cache["errormessage"]
     cache["errormessage"] = ""
     return render_template("signup.html", errormessage=errormessage)
 
 
-@ app.route("/logout")
+@app.route("/logout")
 def logout():
     del session["username"]
     return redirect("/")
 
 
 # Submit form routes
-@ app.route("/loginsubmit", methods=["POST"])
+@app.route("/loginsubmit", methods=["POST"])
 def loginsubmit():
     username = request.form["username"]
     password = request.form["password"]
@@ -116,8 +135,7 @@ def loginsubmit():
         # TODO: invalid username
         cache["errormessage"] = "Käyttäjänimeä ei löydy"
         return redirect("/login")
-    else:
-        hash_value = user.password
+    hash_value = user.password
     if check_password_hash(hash_value, password):
         session["username"] = username
     else:
@@ -126,7 +144,7 @@ def loginsubmit():
     return redirect("/")
 
 
-@ app.route("/signupsubmit", methods=["POST"])
+@app.route("/signupsubmit", methods=["POST"])
 def signupsubmit():
     username = request.form["username"]
     password = request.form["password"]
@@ -134,68 +152,82 @@ def signupsubmit():
     hash_value = generate_password_hash(password)
     sql = text("SELECT id, password FROM users WHERE username=:username")
     result = db.session.execute(sql, {"username": username})
-    user = result.fetchone()
-    if not user:
+    found_user = result.fetchone()
+    if not found_user:
         sql = text(
-            "INSERT INTO users (username, password, userlevel) VALUES (:username, :password, :userlevel)")
-        db.session.execute(
-            sql, {"username": username, "password": hash_value, "userlevel": userlevel})
+            "INSERT INTO users (username, password, userlevel) VALUES (:username, :password, :userlevel)"
+        )
+        db.session.execute(sql, {
+            "username": username,
+            "password": hash_value,
+            "userlevel": userlevel
+        })
         db.session.commit()
         return redirect("/signupsuccess")
-    else:
-        cache["errormessage"] = "Käyttäjänimi {} on jo olemassa. Valitse toinen käyttäjänimi.".format(
-            username)
-        return redirect("/signup")
+    cache["errormessage"] = "Käyttäjänimi {} on jo olemassa. Valitse toinen käyttäjänimi.".format(
+        username)
+    return redirect("/signup")
 
 
-@ app.route("/newconversationsubmit", methods=["POST"])
+@app.route("/newconversationsubmit", methods=["POST"])
 def newconversationsubmit():
     header = request.form.get("convo-header", "")
     content = request.form.get("convo-content", "")
-    category = int(request.form.get("convo-category", 0))
+    convo_category = int(request.form.get("convo-category", 0))
     if header == "":
         cache["content"] = content
         cache["errormessage"] = "Otsikko on pakollinen"
         return redirect("/newconversation")
-    if category == 0:
+    if convo_category == 0:
         cache["content"] = content
         cache["header"] = header
         cache["errormessage"] = "Valitse kategoria"
         return redirect("/newconversation")
-    else:
-        username = session["username"]
-        sql = text(
-            "INSERT INTO conversations (username, header, content, category) VALUES (:username, :header, :content, :category)")
-        db.session.execute(
-            sql, {"username": username, "header": header, "content": content, "category": category})
-        db.session.commit()
-        return redirect("/")
+    username = session["username"]
+    sql = text(
+        "INSERT INTO conversations (username, header, content, category) VALUES (:username, :header, :content, :category)"
+    )
+    db.session.execute(
+        sql,
+        {
+            "username": username,
+            "header": header,
+            "content": content,
+            "category": convo_category,
+        },
+    )
+    db.session.commit()
+    return redirect("/")
 
 
-@ app.route("/replysubmit/<int:id>", methods=["POST"])
-def replysubmit(id):
+@app.route("/replysubmit/<int:thread_id>", methods=["POST"])
+def replysubmit(thread_id):
     content = request.form.get("reply-content", "")
     if content == "":
         cache["content"] = content
         cache["errormessage"] = "Kirjoita vastaus"
-        return redirect(url_for('thread', id=id))
-    else:
-        username = session["username"]
-        content_with_linebreaks = content.replace("\r", "<br>")
-        thread_id = id
-        sql = text(
-            "INSERT INTO replies (username, content, thread_id) VALUES (:username, :content, :thread_id)")
-        db.session.execute(
-            sql, {"username": username, "content": content_with_linebreaks, "thread_id": thread_id})
-        db.session.commit()
-        return redirect(url_for('thread', id=id))
+        return redirect(url_for("thread", thread_id=thread_id))
+    username = session["username"]
+    content_with_linebreaks = content.replace("\r", "<br>")
+    sql = text(
+        "INSERT INTO replies (username, content, thread_id) VALUES (:username, :content, :thread_id)"
+    )
+    db.session.execute(
+        sql,
+        {
+            "username": username,
+            "content": content_with_linebreaks,
+            "thread_id": thread_id,
+        },
+    )
+    db.session.commit()
+    return redirect(url_for("thread", thread_id=thread_id))
 
 
-@ app.route("/replyeditsubmit/<int:id>", methods=["POST"])
+@app.route("/replyeditsubmit/<int:id>", methods=["POST"])
 def replyeditsubmit(id):
     content = request.form.get("reply-content", "")
-    sql = text(
-        "SELECT * from replies where id=:id AND deleted_at IS NULL")
+    sql = text("SELECT * from replies where id=:id AND deleted_at IS NULL")
     result = db.session.execute(sql, {"id": id})
     reply = result.fetchone()
     thread_id = reply.thread_id
@@ -203,19 +235,18 @@ def replyeditsubmit(id):
         cache["content"] = reply.content
         cache["reply_to_edit"] = id
         cache["errormessage"] = "Et voi muokata vastausta tyhjäksi"
-        return redirect(url_for('thread', id=thread_id))
-    else:
-        content_with_linebreaks = content.replace("\r", "<br>")
-        sql = text("UPDATE replies SET content = :content WHERE id = :id")
-        db.session.execute(sql, {"content": content_with_linebreaks, "id": id})
-        db.session.commit()
-        return redirect(url_for('thread', id=thread_id))
+        return redirect(url_for("thread", thread_id=thread_id))
+    content_with_linebreaks = content.replace("\r", "<br>")
+    sql = text("UPDATE replies SET content = :content WHERE id = :id")
+    db.session.execute(sql, {"content": content_with_linebreaks, "id": id})
+    db.session.commit()
+    return redirect(url_for("thread", thread_id=thread_id))
 
 
-@app.route("/deletereply/<int:id>")
-def deletereply(id):
+@app.route("/deletereply/<int:reply_id>")
+def deletereply(reply_id):
     sql = text("SELECT * FROM replies WHERE id=:id")
-    result = db.session.execute(sql, {"id": id})
+    result = db.session.execute(sql, {"id": reply_id})
     reply = result.fetchone()
     if not reply:
         cache["errormessage"] = "Tapahtui virhe. Yritä myöhemmin uudelleen."
@@ -223,15 +254,13 @@ def deletereply(id):
     if reply.username != session["username"]:
         cache["errormessage"] = "Tapahtui virhe. Yritä myöhemmin uudelleen."
         thread_id = reply.thread_id
-        return redirect(url_for('thread', id=thread_id))
-    else:
-        id = reply.id
-        thread_id = reply.thread_id
-        sql = text("UPDATE replies SET deleted_at = :timestamp WHERE id=:id")
-        timestamp = datetime.datetime.now(pytz.timezone("Europe/Helsinki"))
-        db.session.execute(sql, {"id": id, "timestamp": timestamp})
-        db.session.commit()
-        return redirect(url_for('thread', id=thread_id))
+        return redirect(url_for("thread", thread_id=thread_id))
+    thread_id = reply.thread_id
+    sql = text("UPDATE replies SET deleted_at = :timestamp WHERE id=:id")
+    timestamp = datetime.datetime.now(pytz.timezone("Europe/Helsinki"))
+    db.session.execute(sql, {"id": reply_id, "timestamp": timestamp})
+    db.session.commit()
+    return redirect(url_for("thread", thread_id=thread_id))
 
 
 @app.route("/editreply/<int:id>")
@@ -245,10 +274,9 @@ def editreply(id):
     if reply.username != session["username"]:
         cache["errormessage"] = "Tapahtui virhe. Yritä myöhemmin uudelleen."
         thread_id = reply.thread_id
-        return redirect(url_for('thread', id=thread_id))
-    else:
-        cache["content"] = reply.content
-        reply_id = reply.id
-        thread_id = reply.thread_id
-        cache["reply_to_edit"] = reply_id
-        return redirect(url_for('thread', id=thread_id))
+        return redirect(url_for("thread", thread_id=thread_id))
+    cache["content"] = reply.content
+    reply_id = reply.id
+    thread_id = reply.thread_id
+    cache["reply_to_edit"] = reply_id
+    return redirect(url_for("thread", thread_id=thread_id))
